66da43b31ecac1b42ac3aeb582567a91c003fe86
GuXianWN <guxianwn@qq.com>
2023-12-18 21:06:31 +0800

    cache

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -1,20 +1,41 @@
 package com.demo.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.demo.entity.Demo;
+import com.demo.entity.R;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @RequestMapping
 @RestController
 public class DemoController {
-    @GetMapping("1")
-    public Object demo() {
-        return SecurityContextHolder.getContext().getAuthentication();
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @GetMapping("/1")
+    public Demo demo() {
+        Demo demo = new Demo();
+        demo.setR(R.success())
+                .setDate(new Date());
+        redisTemplate.opsForValue().set("1", demo);
+
+        Object obj = redisTemplate.opsForValue().get("1");
+        demo = (Demo) obj;
+        return demo;
     }
 
-    @GetMapping("2")
-    public Object demo2() {
-        return SecurityContextHolder.getContext().getAuthentication();
+    @Cacheable("1")
+    @GetMapping("/2")
+    public Demo demo2() {
+        System.out.println("cache");
+        return new Demo()
+                .setR(R.success())
+                .setDate(new Date());
     }
 }
